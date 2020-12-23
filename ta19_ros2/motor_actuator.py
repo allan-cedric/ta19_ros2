9fa44c6d40f6af9dbae03cc78fe2f6f77b460730
allan-cedric <allan.py3000@gmail.com>
2020-12-22 21:21:54 -0300

    ta19 completo

--- ta19_ros2/motor_actuator.py
+++ ta19_ros2/motor_actuator.py
@@ -16,14 +16,27 @@ import rclpy
 
 from std_msgs.msg import String
 
+
 def direction_callback(msg):
-    node.get_logger().info('Direction: %s' % msg.data)
-    
+    if msg.data == 'w':
+        node.get_logger().info('Robô vai para FRENTE')
+    elif msg.data == 'a':
+        node.get_logger().info('Robô vai pra ESQUERDA')
+    elif msg.data == 's':
+        node.get_logger().info('Robô vai para TRÁS')
+    elif msg.data == 'd':
+        node.get_logger().info('Robô vai para DIREITA')
+    elif msg.data == 'x':
+        node.get_logger().info('Robô PARA')
+    else:
+        node.get_logger().info('Comando inválido')
+
+
 def main(args=None):
     rclpy.init(args=args)
 
     global node
-    node = rclpy.create_node('minimal_subscriber')
+    node = rclpy.create_node('motor_actuator')
 
     subscription = node.create_subscription(
         String, 'motor_command', direction_callback, 10)
